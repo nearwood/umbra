81c3dd3c818c6751f9819a88fffa7bdd1c324e3a
Nick Earwood <nearwood@pm.me>
2021-02-08 22:08:25 -0600

    make ugly board

--- src/App.js
+++ src/App.js
@@ -7,6 +7,115 @@ const UmbraBoard = (props) => {
   return (<>
     <div>Phase: {ctx.phase}</div>
     <div>Current Player: {ctx.currentPlayer}</div>
+    <hr />
+    <div id='board' className='col'>
+      <div id='shipSection'>
+        Ships go here.
+      </div>
+      <div id='rest' className='row'>
+        <div id='rep'>
+          Rep goes here.
+        </div>
+        <div className='right col'>
+          <div id='techTree' className='col'>
+            <div className='tech row'>
+              <div className='techCell' />
+              <div className='techCell' />
+              <div className='techCell' />
+              <div className='techCell' />
+              <div className='techCell' />
+              <div className='techCell' />
+              <div className='techCell' />
+            </div>
+            <div className='tech row'>
+              <div className='techCell' />
+              <div className='techCell' />
+              <div className='techCell' />
+              <div className='techCell' />
+              <div className='techCell' />
+              <div className='techCell' />
+              <div className='techCell' />
+            </div>
+            <div className='tech row'>
+              <div className='techCell' />
+              <div className='techCell' />
+              <div className='techCell' />
+              <div className='techCell' />
+              <div className='techCell' />
+              <div className='techCell' />
+              <div className='techCell' />
+            </div>
+          </div>
+          <div className='row'>
+            <div id='production' className='col'>
+              <div className='row' id='money'>
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+              </div>
+              <div className='row' id='science'>
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+              </div>
+              <div className='row' id='materials'>
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+                <div className='cubeCell' />
+              </div>
+            </div>
+            <div id='buttons' className='row'>
+              <button>EXP</button>
+              <button>INF</button>
+              <button>RES</button>
+              <button>UPG</button>
+              <button>REP</button>
+              <button>MOV</button>
+              <button onClick={() => props.moves.pass()}>PAS</button>
+            </div>
+          </div>
+          <div id='actions' className='row'>
+            <div className='actionCell' />
+            <div className='actionCell' />
+            <div className='actionCell' />
+            <div className='actionCell' />
+            <div className='actionCell' />
+            <div className='actionCell' />
+            <div className='actionCell' />
+            <div className='actionCell' />
+            <div className='actionCell' />
+            <div className='actionCell' />
+            <div className='actionCell' />
+            <div className='actionCell' />
+            <div className='actionCell' />
+          </div>
+        </div>
+      </div>
+    </div>
   </>);
 };
 
