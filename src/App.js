0eb8eea0128bbfd3d11d6e75f05f37a9f54ff861
Nick Earwood <nearwood@pm.me>
2021-02-08 21:17:12 -0600

    stub out turns and shit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import { Client } from 'boardgame.io/react';
 import { Umbra } from './Game';
 
-const App = Client({ game: Umbra });
+const UmbraBoard = (props) => {
+  const { G, ctx } = props;
+
+  return (<>
+    <div>Phase: {ctx.phase}</div>
+    <div>Current Player: {ctx.currentPlayer}</div>
+  </>);
+};
+
+const App = Client({
+  game: Umbra,
+  board: UmbraBoard,
+});
+
 
 export default App;
--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,61 @@
 
-export const Umbra = {
-  setup: () => ({ cells: Array(9).fill(null) }),
+const resetHasPassed = (numPlayers, obj) => {
+  const o = obj || {};
 
-  moves: {
-    clickCell: (G, ctx, id) => {
-      G.cells[id] = ctx.currentPlayer;
+  for (let i = 0; i < numPlayers; ++i) {
+    o[i] = false;
+  }
+
+  return o;
+}
+
+export const Umbra = {
+  setup: (ctx) => ({ hasPassed: resetHasPassed(ctx.numPlayers), shipsAvailableForCombat: 0 }),
+
+  phases: {
+    action: {
+      start: true,
+      moves: {
+        pass: (G, ctx) => {
+          G.hasPassed[ctx.currentPlayer] = true;
+          ctx.events.endTurn();
+        }
+      },
+      endIf: G => Object.values(G.hasPassed).every(p => p),
+      onEnd: (G, ctx) => (resetHasPassed(ctx.numPlayers, G.hasPassed), G),
+      next: 'combat'
     },
-  },
+    combat: {
+      moves: {
+        engage: (G, ctx) => {
+          ctx.events.endTurn();
+        },
+        retreat: (G, ctx, shipType, hex) => { }
+      },
+      endIf: G => G.shipsAvailableForCombat === 0,
+      next: 'upkeep'
+    },
+    upkeep: {
+      moves: {
+        activeColonyShip: () => { },
+        pass: (G, ctx) => {
+          G.hasPassed[ctx.currentPlayer] = true;
+          ctx.events.endTurn();
+        }
+      },
+      endIf: G => Object.values(G.hasPassed).every(p => p),
+      onEnd: (G, ctx) => (resetHasPassed(ctx.numPlayers, G.hasPassed), G),
+      next: 'action'
+    }
+  }
 };
+
+/**
+ * players <=6
+ * do nothing option to end turn
+ * phases: action, combat, upkeep, cleanup
+ *
+ * actions:
+ * explore, influence, research, upgrade, build, move
+ * need to prioritize that ^
+ */
