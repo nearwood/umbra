6930538c4e5eea4e7c746263bb8f56fc1ce92ea4
Nick Earwood <nearwood@pm.me>
2021-02-10 23:01:06 -0600

    Show map ring text

--- src/HexMap.jsx
+++ src/HexMap.jsx
@@ -1,5 +1,14 @@
 import { HexGrid, Layout, Hexagon, Text, Pattern, Path, Hex } from 'react-hexgrid';
 
+const ringText = (ring) => {
+  switch (ring) {
+    default:
+    case 0: return '';
+    case 1: return 'I';
+    case 2: return 'II';
+    case 3: return 'III';
+  }
+}
 
 export const HexMap = ({ props }) => {
   const { G, ctx } = props;
@@ -10,7 +19,8 @@ export const HexMap = ({ props }) => {
         <Layout size={{ x: 10, y: 10 }} flat={true} spacing={1.1} origin={{ x: 0, y: 0 }}>
           {Array.isArray(G.map) && G.map.map(s =>
             <Hexagon q={s.pos.q} r={s.pos.r} s={s.pos.s} className={`ring${s.ring}`}>
-              <Text>{`${s.pos.q}, ${s.pos.r}, ${s.pos.s}`}</Text>
+              {/* <Text>{`${s.pos.q}, ${s.pos.r}, ${s.pos.s}`}</Text> */}
+              <Text>{ringText(s.ring)}</Text>
             </Hexagon>
           )}
         </Layout>
